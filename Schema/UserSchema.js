0e0ac082202e144f5182ba0d65bf0497bef69d18
llama <llama@llamas-MacBook-Pro.local>
2024-11-17 21:13:03 +0545

    Fix: Added comments features and minor refactor

--- Schema/UserSchema.js
+++ Schema/UserSchema.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
   name: String,
   password: String,
+  role: {
+    type: String,
+    default: 'customer'
+  },
   email: { type: String, lowercase: true },
 });
 
